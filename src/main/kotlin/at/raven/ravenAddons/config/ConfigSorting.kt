735910bc4dca482eca1d4a42ffb3ba4542747fc3
raaaaaven <yusuff2007@hotmail.com>
2025-02-01 01:53:28 +0000

    added config check and more category sorting options

--- src/main/kotlin/at/raven/ravenAddons/config/ConfigSorting.kt
+++ src/main/kotlin/at/raven/ravenAddons/config/ConfigSorting.kt
@@ -5,7 +5,7 @@ import gg.essential.vigilance.data.SortingBehavior
 
 class ConfigSorting: SortingBehavior() {
 
-    private val categories: List<String> = listOf("General", "SkyBlock", "Mining", "Developer")
+    private val categories: List<String> = listOf("General", "Pit", "SkyBlock", "Dungeons", "Kuudra", "Slayers", "Events", "Mining", "Farming", "Developer")
 
     override fun getCategoryComparator(): Comparator<in Category> {
         return compareBy { categories.indexOf(it.name) }
--- src/main/kotlin/at/raven/ravenAddons/features/CarePackageHighlighter.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/CarePackageHighlighter.kt
@@ -1,39 +1,35 @@
 package at.raven.ravenAddons.features
 
+import at.raven.ravenAddons.config.ravenAddonsConfig
 import at.raven.ravenAddons.event.render.container.ContainerBackgroundDrawEvent
 import at.raven.ravenAddons.loadmodule.LoadModule
 import at.raven.ravenAddons.utils.InventoryUtils.getUpperItems
 import at.raven.ravenAddons.utils.RenderUtils.highlight
-import at.raven.ravenAddons.utils.RenderUtils.withAlpha
 import net.minecraft.client.gui.inventory.GuiChest
 import net.minecraft.inventory.ContainerChest
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import java.awt.Color
 
 @LoadModule
 object CarePackageHighlighter {
     private val importantItems = setOf(
-        "Enchanted Book",
-        "Diamond",
-        "Emerald",
-        "Gold Ingot",
-        "Iron Ingot",
-        "Golden Apple",
-        "Notch Apple",
-        "Saddle",
-        "Name Tag",
-        "Horse Armor",
-        "Obsidian"
+        "Mystic Sword",
+        "Mystic Bow",
+        "Fresh Red Pants",
+        "Fresh Orange Pants",
+        "Fresh Yellow Pants",
+        "Fresh Green Pants",
+        "Fresh Blue Pants"
     )
 
     @SubscribeEvent
     fun onContainerForeground(event: ContainerBackgroundDrawEvent) {
+        if (!ravenAddonsConfig.carePackageHighlighter) return
         if (event.gui !is GuiChest) return
         val inventory = event.gui.inventorySlots as ContainerChest
 
         for ((slot, stack) in inventory.getUpperItems()) {
             if (importantItems.any { stack?.displayName?.contains(it) == true }) {
-                slot highlight Color.GREEN.withAlpha(100)
+                slot highlight ravenAddonsConfig.carePackageHighlighterColour
             }
         }
     }
