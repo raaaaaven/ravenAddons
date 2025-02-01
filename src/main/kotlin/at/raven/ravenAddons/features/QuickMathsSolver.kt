9dc92de15892f9fe624b50b4300fb467d58ac03a
martimavocado <martim.cavaco@tutanota.com>
2025-02-01 21:16:47 +0000

    cleanup quickmaths solver

--- src/main/kotlin/at/raven/ravenAddons/features/QuickMathsSolver.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/QuickMathsSolver.kt
@@ -2,30 +2,34 @@ package at.raven.ravenAddons.features
 
 import at.raven.ravenAddons.config.ravenAddonsConfig
 import at.raven.ravenAddons.loadmodule.LoadModule
+import at.raven.ravenAddons.utils.Calculator.calc
 import at.raven.ravenAddons.utils.ChatUtils
+import at.raven.ravenAddons.utils.RegexUtils.matchMatcher
 import at.raven.ravenAddons.utils.TitleManager
 import net.minecraftforge.client.event.ClientChatReceivedEvent
-import javax.script.ScriptEngineManager
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
 
 @LoadModule
 object QuickMathsSolver {
+    private val pattern = "QUICK MATHS! Solve: (?<expression>.+)".toPattern()
+
+    @SubscribeEvent
     fun onChat(event: ClientChatReceivedEvent) {
         if (!ravenAddonsConfig.quickMathsSolver) return
-
         val message = event.message.unformattedText
-        val regex = Regex("QUICK MATHS! Solve: (.+)")
-        val match = regex.find(message) ?: return
-        val expression = match.groupValues[1].replace("x", "*")
 
-        try {
-            val engine = ScriptEngineManager().getEngineByName("JavaScript")
-            val result = engine.eval(expression)
+        pattern.matchMatcher(message) {
+            try {
+                val expression = group("expression") ?: return
+                val result = expression.calc().toString().removeSuffix(".0")
 
-            ChatUtils.chat("§7The §d§lQUICK MATHS! §r§7answer is §f§l$result§r§7!")
-            TitleManager.setTitle("§f§l$result", "§d§lQUICK MATHS!", 2.5.seconds, 0.5.seconds, 0.5.seconds)
-        } catch (e: Exception) {
-            ChatUtils.warning("Failed to solve QUICK MATHS! (${e.message})")
+                ChatUtils.chat("§7The §d§lQUICK MATHS! §r§7answer is §f§l$result§r§7!")
+                TitleManager.setTitle("§f§l$result", "§d§lQUICK MATHS!", 2.5.seconds, 0.5.seconds, 0.5.seconds)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ChatUtils.warning("Failed to solve QUICK MATHS! (${e.message})")
+            }
         }
     }
 }
