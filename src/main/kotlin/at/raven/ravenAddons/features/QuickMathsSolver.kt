245b3b6821a5190778a4773747f1c7a3e40c44d4
raaaaaven <yusuff2007@hotmail.com>
2025-02-01 20:39:21 +0000

    SubscribeEvent

--- src/main/kotlin/at/raven/ravenAddons/features/QuickMathsSolver.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/QuickMathsSolver.kt
@@ -5,11 +5,13 @@ import at.raven.ravenAddons.loadmodule.LoadModule
 import at.raven.ravenAddons.utils.ChatUtils
 import at.raven.ravenAddons.utils.TitleManager
 import net.minecraftforge.client.event.ClientChatReceivedEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import javax.script.ScriptEngineManager
 import kotlin.time.Duration.Companion.seconds
 
 @LoadModule
 object QuickMathsSolver {
+    @SubscribeEvent
     fun onChat(event: ClientChatReceivedEvent) {
         if (!ravenAddonsConfig.quickMathsSolver) return
 
