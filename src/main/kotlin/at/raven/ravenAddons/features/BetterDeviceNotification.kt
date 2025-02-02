ef2fed84e667750580b977f9a1d7b12d4a0bcfbf
martimavocado <martim.cavaco@tutanota.com>
2025-02-02 02:51:50 +0000

    device notification cleanup

--- src/main/kotlin/at/raven/ravenAddons/features/BetterDeviceNotification.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/BetterDeviceNotification.kt
@@ -16,7 +16,7 @@ import kotlin.time.Duration.Companion.seconds
 object BetterDeviceNotification {
 
     // REGEX TEST: §r§r§r§aGillsplash§r§a completed a device! (§r§c6§r§a/7)§r
-    private val devicePattern = "^§r§r§r(?<ign>.+)§r§a completed a device! (?<number>.+)\$".toPattern()
+    private val devicePattern = "^§r§r§r(?<ign>.+)§r§a completed a device! (?<number>.+)$".toPattern()
 
     @SubscribeEvent
     fun onChat(event: ClientChatReceivedEvent) {
@@ -24,15 +24,13 @@ object BetterDeviceNotification {
 
         devicePattern.matchMatcher(event.message.formattedText.removeColors()) {
             val ign = group("ign")
-            val numbers = group("numbers")
-
             if (ign != PlayerUtils.playerName) return
 
             ChatUtils.debug("betterDeviceNotification: attempting to send title and subtitle for $ign")
 
             TitleManager.setTitle(
-                "${ravenAddonsConfig.betterDeviceNotificationTitle}",
-                "${ravenAddonsConfig.betterDeviceNotificationSubTitle}",
+                ravenAddonsConfig.betterDeviceNotificationTitle,
+                ravenAddonsConfig.betterDeviceNotificationSubTitle,
                 1.5.seconds,
                 0.seconds,
                 0.seconds)
