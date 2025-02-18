f90880bf929b9e31fe7309fc1e44f65a6a7499e7
raaaaaven <yusuff2007@hotmail.com>
2025-02-18 19:53:59 +0000

    add delay to account for lag

--- src/main/kotlin/at/raven/ravenAddons/features/BetterDeviceNotification.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/BetterDeviceNotification.kt
@@ -4,12 +4,14 @@ import at.raven.ravenAddons.config.ravenAddonsConfig
 import at.raven.ravenAddons.data.HypixelGame
 import at.raven.ravenAddons.data.HypixelGame.Companion.isNotPlaying
 import at.raven.ravenAddons.loadmodule.LoadModule
+import at.raven.ravenAddons.ravenAddons
 import at.raven.ravenAddons.utils.ChatUtils
 import at.raven.ravenAddons.utils.PlayerUtils
 import at.raven.ravenAddons.utils.RegexUtils.matchMatcher
 import at.raven.ravenAddons.utils.SoundUtils
 import at.raven.ravenAddons.utils.StringUtils.removeColors
 import at.raven.ravenAddons.utils.TitleManager
+import kotlinx.coroutines.delay
 import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
@@ -33,13 +35,16 @@ object BetterDeviceNotification {
 
             ChatUtils.debug("betterDeviceNotification: attempting to send title and subtitle for $ign")
 
-            TitleManager.setTitle(
-                "${ravenAddonsConfig.betterDeviceNotificationTitle}",
-                "${ravenAddonsConfig.betterDeviceNotificationSubTitle}",
-                1.5.seconds,
-                0.seconds,
-                0.seconds)
-            SoundUtils.pling()
+            ravenAddons.launchCoroutine {
+                delay(5)
+                TitleManager.setTitle(
+                    "${ravenAddonsConfig.betterDeviceNotificationTitle}",
+                    "${ravenAddonsConfig.betterDeviceNotificationSubTitle}",
+                    1.5.seconds,
+                    0.seconds,
+                    0.seconds)
+                SoundUtils.pling()
+            }
         }
     }
 }
