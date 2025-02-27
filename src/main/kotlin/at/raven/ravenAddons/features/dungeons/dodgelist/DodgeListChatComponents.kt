e77adac3efe0b6d811125bf5605a95373ae174d0
martimavocado <39881008+martimavocado@users.noreply.github.com>
2025-02-27 19:15:36 +0000

    how did this happen

--- src/main/kotlin/at/raven/ravenAddons/features/dungeons/dodgelist/DodgeListChatComponents.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/dungeons/dodgelist/DodgeListChatComponents.kt
@@ -4,7 +4,6 @@ import net.minecraft.event.ClickEvent
 import net.minecraft.event.HoverEvent
 import net.minecraft.util.ChatComponentText
 import net.minecraft.util.IChatComponent
-import sun.audio.AudioPlayer.player
 
 object DodgeListChatComponents {
     fun getLineComponent(withNewLine: Boolean = true) =
@@ -13,37 +12,37 @@ object DodgeListChatComponents {
 
     val prefixComponent = ChatComponentText("§8[§cRA§8] ")
 
-    val announceComponent: ChatComponentText get() {
+    fun getAnnounceComponent(playerName: String): ChatComponentText {
         val component = ChatComponentText("§9§l[ANNOUNCE] ")
         component.chatStyle.chatClickEvent =
-            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/pc [RA] $player is on the dodge list!")
+            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/pc [RA] $playerName is on the dodge list!")
         component.chatStyle.chatHoverEvent =
             HoverEvent(HoverEvent.Action.SHOW_TEXT, ChatComponentText("§7Click here to announce to party chat."))
 
         return component
     }
-    val kickComponent: ChatComponentText get() {
+    fun getKickComponent(playerName: String): ChatComponentText {
         val component = ChatComponentText("§c§l[KICK] ")
         component.chatStyle.chatClickEvent =
-            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/ra-action-kick $player")
+            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/ra-action-kick $playerName")
         component.chatStyle.chatHoverEvent =
             HoverEvent(HoverEvent.Action.SHOW_TEXT, ChatComponentText("§7Click here to kick the dodged user."))
 
         return component
     }
-    val blockComponent: ChatComponentText get() {
+    fun getBlockComponent(playerName: String): ChatComponentText {
         val component = ChatComponentText("§7§l[BLOCK] ")
         component.chatStyle.chatClickEvent =
-            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/block add $player")
+            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/block add $playerName")
         component.chatStyle.chatHoverEvent =
             HoverEvent(HoverEvent.Action.SHOW_TEXT, ChatComponentText("§7Click here to block the dodged user."))
 
         return component
     }
-    val removeComponent: ChatComponentText get() {
+    fun getRemoveComponent(playerName: String): ChatComponentText {
         val component = ChatComponentText("§c§l[REMOVE] \n")
         component.chatStyle.chatClickEvent =
-            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/dodge remove $player")
+            ClickEvent(ClickEvent.Action.RUN_COMMAND, "/dodge remove $playerName")
         component.chatStyle.chatHoverEvent =
             HoverEvent(HoverEvent.Action.SHOW_TEXT, ChatComponentText("§7Click here to remove the user from the dodge list."))
 
