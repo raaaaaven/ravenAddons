72b31edec6aed09ae4c6a4bc9a85874b70f95249
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2025-04-11 19:53:39 +0200

    Create ServerTickEvent

---
+++ src/main/kotlin/at/raven/ravenAddons/event/managers/ServerTickManager.kt
@@ -0,0 +1,21 @@
+package at.raven.ravenAddons.event.managers
+
+import at.raven.ravenAddons.event.PacketReceivedEvent
+import at.raven.ravenAddons.event.ServerTickEvent
+import at.raven.ravenAddons.loadmodule.LoadModule
+import at.raven.ravenAddons.utils.EventUtils.post
+import net.minecraft.network.play.server.S32PacketConfirmTransaction
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+@LoadModule
+object ServerTickManager {
+    @SubscribeEvent
+    fun onPacketRecieved(event: PacketReceivedEvent) {
+        if (event.packet !is S32PacketConfirmTransaction) return
+        val packet: S32PacketConfirmTransaction = event.packet
+
+        if (packet.actionNumber > 0) return
+
+        ServerTickEvent().post()
+    }
+}
