78410271f7f5b95be10d72db7d1e4161fd6bf251
Empa <itsempa@users.noreply.github.com>
2025-05-02 19:23:58 +0200

    fix: oopsie

--- src/main/kotlin/at/raven/ravenAddons/data/commands/CommandBuilder.kt
+++ src/main/kotlin/at/raven/ravenAddons/data/commands/CommandBuilder.kt
@@ -6,7 +6,7 @@ class CommandBuilder(val name: String) {
     var hidden = false
     var category = CommandCategory.NORMAL
     private var autoComplete: ((Array<String>) -> List<String>) = { listOf() }
-    private var callback: (Array<String>) -> Unit = {}
+    var callback: (Array<String>) -> Unit = {}
 
     fun callback(callback: (Array<String>) -> Unit) {
         this.callback = callback
