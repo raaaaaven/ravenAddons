d8794b71a8db0fa4ff25eb5e871ba4620a4a734d
Empa <itsempa@users.noreply.github.com>
2025-05-03 10:40:23 +0200

    style: override isCancelable method instead of using Cancelable annotation

--- src/main/kotlin/at/raven/ravenAddons/event/base/CancellableRavenEvent.kt
+++ src/main/kotlin/at/raven/ravenAddons/event/base/CancellableRavenEvent.kt
@@ -1,9 +1,7 @@
 package at.raven.ravenAddons.event.base
 
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.fml.common.eventhandler.Cancelable
 
-@Cancelable
 abstract class CancellableRavenEvent : RavenEvent() {
     override fun post(): Boolean {
         MinecraftForge.EVENT_BUS.post(this)
@@ -12,4 +10,5 @@ abstract class CancellableRavenEvent : RavenEvent() {
     fun cancel() {
         this.isCanceled = true
     }
+    override fun isCancelable(): Boolean = true
 }
