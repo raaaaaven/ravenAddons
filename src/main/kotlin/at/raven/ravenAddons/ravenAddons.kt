694d5dcc15c15ab10e5c4c5ddf294e83ce2a335f
raaaaaven <yusuff2007@hotmail.com>
2025-05-04 14:46:10 +0100

    version bump

--- src/main/kotlin/at/raven/ravenAddons/ravenAddons.kt
+++ src/main/kotlin/at/raven/ravenAddons/ravenAddons.kt
@@ -47,7 +47,7 @@ class ravenAddons {
 
     @LoadModule
     companion object{
-        const val MOD_VERSION = "1.9.2"
+        const val MOD_VERSION = "1.9.3"
         const val MOD_ID = "ravenAddons"
         val modVersion get() = UpdateManager.modVersionNumber(MOD_VERSION)
 
