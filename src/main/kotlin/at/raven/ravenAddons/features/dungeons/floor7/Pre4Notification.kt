140b56532b8d2faafdde9a7903c457f7919f1377
raaaaaven <yusuff2007@hotmail.com>
2025-05-12 15:17:08 +0100

    fix config not saving

--- src/main/kotlin/at/raven/ravenAddons/features/dungeons/floor7/Pre4Notification.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/dungeons/floor7/Pre4Notification.kt
@@ -126,7 +126,8 @@ object Pre4Notification {
                 // TO-DO: Calculate the difference between new and old pb.
                 if ( timeElapsed < personalBest) {
                     ChatUtils.chat("Pre 4 took §f${formatTicks(timeElapsed)}§7. §d§l(NEW PB) §8(${formatTicks(personalBest)})")
-                    personalBest = timeElapsed
+                    ravenAddonsConfig.pre4PersonalBestNumber = timeElapsed
+                    ravenAddonsConfig.markDirty()
                 } else {
                     ChatUtils.chat("Pre 4 took §f${formatTicks(timeElapsed)}§7. §8(${formatTicks(personalBest)})")
                 }
--- src/main/kotlin/at/raven/ravenAddons/features/dungeons/floor7/SimonSaysPersonalBestTracker.kt
+++ src/main/kotlin/at/raven/ravenAddons/features/dungeons/floor7/SimonSaysPersonalBestTracker.kt
@@ -53,7 +53,8 @@ object SimonSaysPersonalBestTracker {
                 // TO-DO: Calculate the difference between new and old pb.
                 if ( timeElapsed < personalBest) {
                     ChatUtils.chat("Simon Says took §f${formatTicks(timeElapsed)}§7. §d§l(NEW PB) §8(${formatTicks(personalBest)})")
-                    personalBest = timeElapsed
+                    ravenAddonsConfig.simonSaysPersonalBestNumber = timeElapsed
+                    ravenAddonsConfig.markDirty()
                 } else {
                     ChatUtils.chat("Simon Says took §f${formatTicks(timeElapsed)}§7. §8(${formatTicks(personalBest)})")
                 }
