3cab435f346917e8c00923a5439ceb2a9fe34df4
raaaaaven <yusuff2007@hotmail.com>
2025-06-08 21:35:44 +0100

    data class

--- src/main/kotlin/at/raven/ravenAddons/commands/A7XCommand.kt
+++ src/main/kotlin/at/raven/ravenAddons/commands/A7XCommand.kt
@@ -12,181 +12,187 @@ import kotlin.time.Duration.Companion.seconds
 
 @LoadModule
 object A7XCommand {
-    private val albums = mapOf(
-        "Sounding the Seventh Trumpet" to listOf(
-            "To End the Rapture",
-            "Turn the Other Way",
-            "Darkness Surrounding",
-            "The Art of Subconscious Illusion",
-            "We Come Out at Night",
-            "Lips of Deceit",
-            "Warmness on the Soul",
-            "An Epic of Time Wasted",
-            "Breaking Their Hold",
-            "Forgotten Faces",
-            "Thick and Thin",
-            "Streets",
-            "Shattered by Broken Dreams"
-        ),
 
-        "Waking The Fallen" to listOf(
-            "Unholy Confessions",
-            "Chapter Four",
-            "Remenissions",
-            "Desecrate Through Reverence",
-            "Eternal Rest",
-            "Second Heartbeat",
-            "Radiant Eclipse",
-            "I Won't See You Tonight Part 1",
-            "I Won't See You Tonight Part 2",
-            "Clairvoyant Disease",
-            "And All Things Will End"
-        ),
+    data class Album(
+        val name: String,
+        val color: String,
+        val songs: List<String>
+    )
 
-        "City of Evil" to listOf(
-            "Beast and the Harlot",
-            "Burn it Down",
-            "Blinded in Chains",
-            "Bat Country",
-            "Trashed and Scattered",
-            "Seize the Day",
-            "Sidewinder",
-            "The Wicked End",
-            "Strength of the World",
-            "Betrayed",
-            "M.I.A"
+    private val albums = listOf(
+        Album(
+            "Sounding the Seventh Trumpet", "§d", listOf(
+                "To End the Rapture",
+                "Turn the Other Way",
+                "Darkness Surrounding",
+                "The Art of Subconscious Illusion",
+                "We Come Out at Night",
+                "Lips of Deceit",
+                "Warmness on the Soul",
+                "An Epic of Time Wasted",
+                "Breaking Their Hold",
+                "Forgotten Faces",
+                "Thick and Thin",
+                "Streets",
+                "Shattered by Broken Dreams"
+            )
         ),
-
-        "Avenged Sevenfold" to listOf(
-            "Critical Acclaim",
-            "Almost Easy",
-            "Scream",
-            "Afterlife",
-            "Gunslinger",
-            "Unbound (The Wild Ride)",
-            "Brompton Cocktail",
-            "Lost",
-            "A Little Piece of Heaven",
-            "Dear God"
+        Album(
+            "Waking The Fallen", "§8", listOf(
+                "Unholy Confessions",
+                "Chapter Four",
+                "Remenissions",
+                "Desecrate Through Reverence",
+                "Eternal Rest",
+                "Second Heartbeat",
+                "Radiant Eclipse",
+                "I Won't See You Tonight Part 1",
+                "I Won't See You Tonight Part 2",
+                "Clairvoyant Disease",
+                "And All Things Will End"
+            )
         ),
-
-        "Nightmare" to listOf(
-            "Nightmare",
-            "Welcome to the Family",
-            "Danger Line",
-            "Buried Alive",
-            "Natural Born Killer",
-            "So Far Away",
-            "God Hates Us",
-            "Victim",
-            "Tonight the World Dies",
-            "Fiction",
-            "Save Me"
+        Album(
+            "City of Evil", "§c", listOf(
+                "Beast and the Harlot",
+                "Burn it Down",
+                "Blinded in Chains",
+                "Bat Country",
+                "Trashed and Scattered",
+                "Seize the Day",
+                "Sidewinder",
+                "The Wicked End",
+                "Strength of the World",
+                "Betrayed",
+                "M.I.A"
+            )
         ),
-
-        "Hail to the King" to listOf(
-            "Shepherd of Fire",
-            "Hail to the King",
-            "Doing Time",
-            "This Means War",
-            "Requiem",
-            "Crimson Day",
-            "Heretic",
-            "Coming Home",
-            "Planets",
-            "Acid Rain"
+        Album(
+            "Avenged Sevenfold", "§f", listOf(
+                "Critical Acclaim",
+                "Almost Easy",
+                "Scream",
+                "Afterlife",
+                "Gunslinger",
+                "Unbound (The Wild Ride)",
+                "Brompton Cocktail",
+                "Lost",
+                "A Little Piece of Heaven",
+                "Dear God"
+            )
         ),
-
-        "The Stage (Deluxe Edition)" to listOf(
-            "The Stage",
-            "Paradigm",
-            "Sunny Disposition",
-            "God Damn",
-            "Creating God",
-            "Angels",
-            "Simulation",
-            "Higher",
-            "Roman Sky",
-            "Fermi Paradox",
-            "Exist",
-            "Dose",
-            "Retrovertigo",
-            "Malagueña Salerosa",
-            "Runaway",
-            "As Tears Go By",
-            "Wish You Were Here",
-            "God Only Knows"
+        Album(
+            "Nightmare", "§9", listOf(
+                "Nightmare",
+                "Welcome to the Family",
+                "Danger Line",
+                "Buried Alive",
+                "Natural Born Killer",
+                "So Far Away",
+                "God Hates Us",
+                "Victim",
+                "Tonight the World Dies",
+                "Fiction",
+                "Save Me"
+            )
         ),
-
-        "Black Reign" to listOf(
-            "Mad Hatter",
-            "Carry On",
-            "Not Ready to Die - From \"Call of the Dead\"",
-            "Jade Helm - Instrumental"
+        Album(
+            "Hail to the King", "§7", listOf(
+                "Shepherd of Fire",
+                "Hail to the King",
+                "Doing Time",
+                "This Means War",
+                "Requiem",
+                "Crimson Day",
+                "Heretic",
+                "Coming Home",
+                "Planets",
+                "Acid Rain"
+            )
         ),
-
-        "Diamonds in the Rough" to listOf(
-            "Demons",
-            "Girl I Know",
-            "Crossroads",
-            "Flash of the Blade",
-            "Until the End",
-            "Tension",
-            "Walk",
-            "The Fight",
-            "Dancing Dead",
-            "St. James",
-            "Set Me Free",
-            "4:00 AM",
-            "Lost It All",
-            "Paranoid"
+        Album(
+            "The Stage (Deluxe Edition)", "§5", listOf(
+                "The Stage",
+                "Paradigm",
+                "Sunny Disposition",
+                "God Damn",
+                "Creating God",
+                "Angels",
+                "Simulation",
+                "Higher",
+                "Roman Sky",
+                "Fermi Paradox",
+                "Exist",
+                "Dose",
+                "Retrovertigo",
+                "Malagueña Salerosa",
+                "Runaway",
+                "As Tears Go By",
+                "Wish You Were Here",
+                "God Only Knows"
+            )
         ),
-
-        "Live in the LBC" to listOf(
-            "Critical Acclaim - Live",
-            "Second Heartbeat - Live",
-            "Afterlife - Live",
-            "Beast and the Harlot - Live",
-            "Scream - Live",
-            "Seize the Day - Live",
-            "Walk - Live",
-            "Bat Country - Live",
-            "Almost Easy - Live",
-            "Gunslinger - Live",
-            "Unholy Confessions - Live",
-            "A Little Piece of Heaven - Live"
+        Album(
+            "Black Reign", "§4", listOf(
+                "Mad Hatter",
+                "Carry On",
+                "Not Ready to Die - From \"Call of the Dead\"",
+                "Jade Helm - Instrumental"
+            )
         ),
-
-        "Life Is But a Dream..." to listOf(
-            "Game Over",
-            "Mattel",
-            "Nobody",
-            "We Love You",
-            "Cosmic",
-            "Beautiful Morning",
-            "Easier",
-            "G",
-            "(O)rdinary",
-            "(D)eath",
-            "Life Is But a Dream..."
+        Album(
+            "Diamonds in the Rough", "§2", listOf(
+                "Demons",
+                "Girl I Know",
+                "Crossroads",
+                "Flash of the Blade",
+                "Until the End",
+                "Tension",
+                "Walk",
+                "The Fight",
+                "Dancing Dead",
+                "St. James",
+                "Set Me Free",
+                "4:00 AM",
+                "Lost It All",
+                "Paranoid"
+            )
+        ),
+        Album(
+            "Live in the LBC", "§a", listOf(
+                "Critical Acclaim - Live",
+                "Second Heartbeat - Live",
+                "Afterlife - Live",
+                "Beast and the Harlot - Live",
+                "Scream - Live",
+                "Seize the Day - Live",
+                "Walk - Live",
+                "Bat Country - Live",
+                "Almost Easy - Live",
+                "Gunslinger - Live",
+                "Unholy Confessions - Live",
+                "A Little Piece of Heaven - Live"
+            )
+        ),
+        Album(
+            "Life Is But a Dream...", "§e", listOf(
+                "Game Over",
+                "Mattel",
+                "Nobody",
+                "We Love You",
+                "Cosmic",
+                "Beautiful Morning",
+                "Easier",
+                "G",
+                "(O)rdinary",
+                "(D)eath",
+                "Life Is But a Dream..."
+            )
         )
     )
 
-    private val colours = mapOf(
-        "Sounding the Seventh Trumpet" to "§d",
-        "Waking The Fallen" to "§8",
-        "City of Evil" to "§c",
-        "Avenged Sevenfold" to "§f",
-        "Nightmare" to "§9",
-        "Hail to the King" to "§7",
-        "The Stage (Deluxe Edition)" to "§5",
-        "Black Reign" to "§4",
-        "Diamonds in the Rough" to "§2",
-        "Live in the LBC" to "§a",
-        "Life Is But a Dream..." to "§e"
-    )
-
-    private val songs: List<Pair<String, String>> = albums.flatMap { (album, songs) -> songs.map { song -> album to song }}
+    private val songs: List<Pair<Album, String>> =
+        albums.flatMap { album -> album.songs.map { song -> album to song } }
 
     @SubscribeEvent
     fun onCommandRegistration(event: CommandRegistrationEvent) {
@@ -210,10 +216,9 @@ object A7XCommand {
             var currentDelay = minimum
             repeat(rolls) { i ->
                 val (album, song) = songs.random()
-                val colour = colours[album] ?: "§f"
                 TitleManager.setTitle(
-                    "$colour$song",
-                    "$colour$album",
+                    "${album.color}$song",
+                    "${album.color}${album.name}",
                     1.seconds,
                     0.seconds,
                     0.seconds
@@ -223,16 +228,15 @@ object A7XCommand {
                 currentDelay += delay
             }
             val (album, song) = songs.random()
-            val colour = colours[album] ?: "§f"
             TitleManager.setTitle(
-                "$colour$song",
-                "$colour$album",
+                "${album.color}$song",
+                "${album.color}${album.name}",
                 2.5.seconds,
                 0.5.seconds,
                 0.5.seconds
             )
             SoundUtils.playSound("random.levelup", 1.0f, 1.0f)
-            ChatUtils.chat("Your Avenged Sevenfold song is $colour$song §7from $colour$album§7.")
+            ChatUtils.chat("Your Avenged Sevenfold song is ${album.color}$song §7from ${album.color}${album.name}§7.")
         }
     }
 }
